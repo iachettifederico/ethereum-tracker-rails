1441d605e7d3cc5a3e34f55d9889763de5b7e572
Federico Iachetti <iachetti.federico@gmail.com>
2022-02-05 20:47:57 -0300

    Updating seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,38 @@ addresses = %w[
 ]
 
 addresses.each do |address|
+  puts "Tracking address " + address.yellow
   Tracker.new.track!(address)
 end
+
+gateway = EtherscanGateway.new
+
+puts "Adding transactions to " + addresses[0].yellow
+gateway.deposit!(addresses[0], amount: 5000, confirmed: true)
+gateway.deposit!(addresses[0], amount: 4000, confirmed: false)
+gateway.deduct!(addresses[0],  amount: 3000, confirmed: true)
+gateway.deduct!(addresses[0],  amount: 2000, confirmed: false)
+
+puts "Adding transactions to " + addresses[1].yellow
+gateway.deposit!(addresses[1], amount: 1000, confirmed: true)
+gateway.deposit!(addresses[1], amount: 100, confirmed: false)
+gateway.deduct!(addresses[1],  amount: 500, confirmed: true)
+gateway.deduct!(addresses[1],  amount: 200, confirmed: false)
+
+puts "Adding transactions to " + addresses[2].yellow
+gateway.deposit!(addresses[2], amount: 5000, confirmed: true)
+gateway.deposit!(addresses[2], amount: 3000, confirmed: false)
+gateway.deduct!(addresses[2],  amount: 1000, confirmed: true)
+gateway.deduct!(addresses[2],  amount: 2000, confirmed: false)
+
+puts "Adding transactions to " + addresses[3].yellow
+gateway.deposit!(addresses[3], amount: 6789, confirmed: true)
+gateway.deposit!(addresses[3], amount: 3456, confirmed: false)
+gateway.deduct!(addresses[3],  amount: 1234, confirmed: true)
+gateway.deduct!(addresses[3],  amount: 1212, confirmed: false)
+
+puts "Adding transactions to " + addresses[4].yellow
+gateway.deposit!(addresses[4], amount: 10000, confirmed: true)
+gateway.deposit!(addresses[4], amount: 4000, confirmed: false)
+gateway.deduct!(addresses[4],  amount: 8000, confirmed: true)
+gateway.deduct!(addresses[4],  amount: 1000, confirmed: false)
